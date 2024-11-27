1fae33dea8b8f799ef98d2eb7fc9a7663b0b7a40
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-27 00:57:08 -0300

    test: adicionando o código teste inicial do projeto

---
+++ src/test/java/br/com/silvio/gerenciadorDeTarefas_back_end/GerenciadorDeTarefasBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.silvio.gerenciadorDeTarefas_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeTarefasBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

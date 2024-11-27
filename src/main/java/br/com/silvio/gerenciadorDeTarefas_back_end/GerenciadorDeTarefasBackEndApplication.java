1647295f57f9c4b792ecfdde3b8ebaac5b40a3ed
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-27 00:56:36 -0300

    feat: adicionando o código que executa o projeto

---
+++ src/main/java/br/com/silvio/gerenciadorDeTarefas_back_end/GerenciadorDeTarefasBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.silvio.gerenciadorDeTarefas_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeTarefasBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeTarefasBackEndApplication.class, args);
+	}
+
+}
